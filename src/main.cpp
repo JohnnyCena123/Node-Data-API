d25af4cc8f1b549cecd195f91ba72123b1d1cb2b
JohnnyCena123 <iamahuman1395@gmail.com>
2025-03-21 14:28:19 +0200

    testing the API

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,13 @@
 #include <Geode/Geode.hpp>
 
+#include "../include/NodeDataAPI.hpp"
+
 using namespace geode::prelude;
 
+class $modify(MyMenuLayer, MenuLayer) {
+    bool init() {
+        if (!MenuLayer::init()) return false;
+        this->addChild(NodeDataAPI::cloneNode<CCSprite*>(this->getChildByID("main-title")));
+        return true;
+    }
+};
