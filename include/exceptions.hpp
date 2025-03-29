0b3bb3994ba247bdab87e4e1de9dcd3d713c26ee
JohnnyCena123 <iamahuman1395@gmail.com>
2025-03-29 18:23:33 +0300

    e

--- include/exceptions.hpp
+++ include/exceptions.hpp
@@ -16,10 +16,10 @@ namespace NodeDataAPI {
 	        log::error("unimplemented function called! function is: {}", unimplementedFunction);
         }
 
-        std::string what() const throw() { 
+        char const* what() const throw() { 
             return fmt::format("Implementation for {} missing! Please notify the developer of the mod.\n"
             "Note for developers of mods depending on Node Data API - please check what functions\n"
-            "are implemented and which are not before you use any of them.\n", m_unimplementedFunction);
+            "are implemented and which are not before you use any of them.\n", m_unimplementedFunction).c_str();
         }
     
     };
